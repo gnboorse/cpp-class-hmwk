fe522375118f94fdff1206922adaad3001712a4d
Gabriel <gnboorse@gmail.com>
2018-03-24 00:12:47 -0400

    updated build script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-for d in */; do
-    cd $d
+dirs="CPA WA_1 WA_2 WA_3 WA_4 WA_5 FinalProject/Part1 FinalProject/Part2 FinalProject/Part3 FinalProject/Part4 FinalProject/Part5"
+home=$PWD
+for d in $dirs; do
+    cd $d/
     echo "Building $d..."
     ./build.sh
     echo "Done"
-    cd ..
+    cd $home
 done
